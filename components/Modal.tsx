d16d435bf51a30e5f7084d5759a82460459d1ced
JakubPaszko2001 <jakubpaszko01@gmail.com>
2022-10-27 23:52:41 +0200

    modal done next zrobic book

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -2,21 +2,23 @@ import React, { useEffect, useState } from "react";
 import MuiModal from "@mui/material/Modal";
 import { modalState, movieState } from "../atoms/modalAtom";
 import { useRecoilState } from "recoil";
-import { Element } from "../types";
+import { Element, Movie } from "../types";
 import ReactPlayer from "react-player/lazy";
 
 const Modal = () => {
   const [showModal, setShowModal] = useRecoilState(modalState);
   const [movieCurrent, setMovieCurrent] = useRecoilState(movieState);
   const [trailer, setTrailer] = useState("");
-  console.log(movieCurrent);
+  const [info, setInfo] = useState();
+  console.log(info);
   useEffect(() => {
     if (!movieCurrent) return;
     async function fetchMovie() {
       const data = await fetch(
         `https://api.themoviedb.org/3/movie/${movieCurrent}?api_key=202c50b1e2676a320151967e42b9cc3b&language=en-US&append_to_response=videos`
       ).then((response) => response.json());
-      console.log(data);
+      // console.log(data);
+      setInfo(data);
       if (data?.videos) {
         const url = data.videos.results.findIndex(
           (element: Element) => element.type === "Trailer"
@@ -30,7 +32,6 @@ const Modal = () => {
   const handleClose = () => {
     setShowModal(false);
   };
-
   return (
     <MuiModal
       className="!top-7 left-0 right-0 z-50 mx-auto w-full max-w-5xl"
@@ -47,11 +48,15 @@ const Modal = () => {
             playing
           />
           <div
-            className="absolute right-5 top-5 !z-40 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-white text-2xl text-black text-white"
+            className="absolute right-5 top-5 !z-40 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-white text-2xl text-black"
             onClick={handleClose}
           >
             X
           </div>
+        </div>
+        <div className="flex w-full flex-col bg-black pt-12 pb-12 font-main text-white">
+          <h1 className="ml-4 text-xl">{info?.title}</h1>
+          <h1 className="ml-4 mr-4 mt-2">{info?.overview}</h1>
         </div>
       </>
     </MuiModal>
